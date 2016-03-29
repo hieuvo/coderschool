33993d735b78a20ac2d0a23d47729c9380ccf717
Hieu Vo <vpmhieu@gmail.com>
2016-03-29 11:27:25 +0700

    add more characters count

--- assignment3/assignment3/NewTweetViewController.swift
+++ assignment3/assignment3/NewTweetViewController.swift
@@ -8,23 +8,42 @@
 
 import UIKit
 
-class NewTweetViewController: UIViewController {
+class NewTweetViewController: UIViewController, UITextViewDelegate {
     
     var tweetsVC: TweetsViewController!
     var tweet: Tweet?
 
+    @IBOutlet weak var charactersCount: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tweetText.delegate = self
+        
         if let user = User.currentUser {
             nameLabel.text = String(user.name!)
             profileImage.loadImageFromURL(user.profileUrl!)
         }
         
+        
         if let replyToUser = tweet?.user {
             tweetText.text = "@\(replyToUser.screenname!) "
+            
+        } else {
+            tweetText.text = ""
         }
+
+        charactersCount.text = String(140 - tweetText.text!.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)) + " chars"
     }
+    
+    func textViewDidChange(textView: UITextView) {
+        charactersCount.text = String(140 - textView.text!.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)) + " chars"
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        return textView.text!.lengthOfBytesUsingEncoding(NSUTF8StringEncoding) < 140
+    }
+    
     @IBAction func cancelAction(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
@@ -46,7 +65,7 @@ class NewTweetViewController: UIViewController {
     }
     
 
-    @IBOutlet weak var tweetText: UITextField!
+    @IBOutlet weak var tweetText: UITextView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     
